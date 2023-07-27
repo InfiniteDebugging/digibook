16fcd2218cb546f24e4a83005874fd50a21c7a98
b4rdarian <con.anasta@gmail.com>
2023-07-27 15:20:59 +0200

    Small visual changes

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,15 +1,23 @@
-import React from 'react'
+import React from "react";
 
-const Card = (props) => {
-    return (
-        <div className='bg-cyan-400 inline-block rounded-lg p-4 m-2 hover:scale-105 drop-shadow-lg border-[.25rem] border-cyan-600 text-center'>
-            <img src={"https://robohash.org/" + (props.id ?? "digiface") + "?set=set2&size=200x200"} alt="digiface" />
-            <div>
-                <h2 className='text-xl font-bold'>{props.name ?? "Your Name"}</h2>
-                <p>{props.email ?? "some.email@digibook.com"}</p>
-            </div>
-        </div>
-    )
-}
+const Card = ({ id, name, email }) => {
+  return (
+    <div className="bg-cyan-400 inline-block rounded-lg p-4 m-2 hover:scale-105 drop-shadow-lg border-[.25rem] border-cyan-600 text-center">
+      <img
+        className="mx-auto"
+        src={
+          "https://robohash.org/" +
+          (id ?? "digiface") +
+          "?set=set2&size=200x200"
+        }
+        alt="digiface"
+      />
+      <div>
+        <h2 className="text-l font-bold">{name ?? "Your Name"}</h2>
+        <p>{email ?? "some.email@digibook.com"}</p>
+      </div>
+    </div>
+  );
+};
 
-export default Card
+export default Card;
--- src/CardList.jsx
+++ src/CardList.jsx
@@ -3,7 +3,9 @@ import Card from './Card'
 
 const CardList = ({ cardList }) => {
     return (
-        <> {cardList.map(card => <Card id={card.id} name={card.name} email={card.email} />)} </>
+        <div> 
+            {cardList.map(card => <Card key={card.id} id={card.id} name={card.name} email={card.email} />)}
+        </div>
     )
 }
 
