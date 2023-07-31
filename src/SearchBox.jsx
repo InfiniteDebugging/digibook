4ecd3e9f7fcacc515904054710c3fae117bef31e
b4rdarian <con.anasta@gmail.com>
2023-07-31 17:23:51 +0200

    Add more styling

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -4,11 +4,11 @@ const SearchBox = ({ placeholder, onSearchChange }) => {
   return (
     <div className="px-2 m-2">
       <input
+        className="p-4 rounded-lg bg-cyan-400 border-[.25rem] border-cyan-600 placeholder:text-slate-500 placeholder:text-center"
         type="search"
         placeholder={placeholder}
         onChange={onSearchChange}
       />
-      ;
     </div>
   );
 };
